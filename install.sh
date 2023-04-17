89741fce1c102044d321cfe9342e648f0b459404
sor4chi <sorakawa0218@gmail.com>
2023-04-18 00:39:43 +0900

    feat(install): "Add installer and uninstaller scripts for git-auto-commit"

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Install the script
+sudo cp -f ./git-auto-commit.sh /usr/local/bin/git-auto-commit
+
+# Make it executable
+sudo chmod +x /usr/local/bin/git-auto-commit
---
+++ uninstall.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Uninstall the script
+sudo rm -f /usr/local/bin/git-auto-commit
